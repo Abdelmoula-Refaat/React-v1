f9030f1b64d28ef38a82fb61a18bab664e4e6734
Abdelmoula-Refaat <refaata791@gmail.com>
2024-08-07 17:24:46 +0300

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,24 +11,26 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <Link className="navbar-brand" to="/">Movies App</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link className="nav-link" to="/movies">Movies</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/login">login</Link>
+            <Link className="nav-link" to="/login">Login</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/Register">Register</Link>
+            <Link className="nav-link" to="/register">Register</Link>
           </li>
-          
           <li className="nav-item">
-                <Link className="nav-link" to="/favorites">
-                  Favorites 
-                  <span className="badge badge-pill badge-primary">{favoriteCount}</span>
-                  <i className="fas fa-star"></i>
-                </Link>
+            <Link className="nav-link" to="/favorites">
+              Favorites 
+              <span className="badge badge-pill badge-primary">{favoriteCount}</span>
+              <i className="fas fa-star"></i>
+            </Link>
           </li>
         </ul>
         <form className="form-inline ms-auto my-2 my-lg-0">
